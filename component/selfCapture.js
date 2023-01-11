ae234ac2a8f6d0839590b16a92b976a85504e488
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2023-01-11 15:46:46 +0900

    fix css and add function strucks

--- component/selfCapture.js
+++ component/selfCapture.js
@@ -3,3 +3,8 @@ import canvas from "https://html2canvas.hertzen.com/dist/html2canvas.min.js"
 canvas(document.querySelector("#capture")).then(canvas => {
     document.body.appendChild(canvas)
 });
+
+//마우스 클릭으로 함수 실행
+
+
+//단축키로 함수 실행 (Alt+!)
