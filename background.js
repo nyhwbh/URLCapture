189a8d5a413fcfea10445c126eacc65dcbf8e3ca
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2023-01-12 07:50:30 +0900

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ whale.browserAction.onClicked.addListener(() => {
   whale.tabs.create({
       url: `http://news.naver.com/`
   });
-})whale.browserAction.onClicked.addListener(() => {
+})
+whale.browserAction.onClicked.addListener(() => {
     whale.tabs.create({
         url: `http://news.naver.com/`
     });
